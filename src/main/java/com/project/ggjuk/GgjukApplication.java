338e3f7e8cc1b6fad9e4978a20d61b231388371c
Bisi3asi <kueeng8008@naver.com>
2024-01-10 07:37:06 +0900

    init: 프로젝트 init

---
+++ src/main/java/com/project/ggjuk/GgjukApplication.java
@@ -0,0 +1,13 @@
+package com.project.ggjuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgjukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgjukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ggjuk/GgjukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ggjuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgjukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
